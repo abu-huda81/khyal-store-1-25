fad2ac94b2f5d7480f8ce37b626b5cf40f4ad369
abu-huda81 <abuhuda51020@gmail.com>
2025-01-13 01:32:24 +0300

    adjusting tabbar

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -1,102 +1,98 @@
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  useWindowDimensions,
-  LayoutChangeEvent,
-} from 'react-native'
-
-import { BottomTabBarProps } from '@react-navigation/bottom-tabs'
-// import { BottomTabBarProps } from "expo-router/node_modules/@react-navigation/bottom-tabs";
-import TabBarButton from './TabBarButton'
-import { Colors } from '@/constants/Colors'
-import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
-import { useEffect, useState } from 'react'
+import { View, StyleSheet, LayoutChangeEvent } from "react-native";
+import { BottomTabBarProps } from "@react-navigation/bottom-tabs";
+import TabBarButton from "./TabBarButton";
+import { Colors } from "@/constants/Colors";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import { useEffect, useState } from "react";
 
 export function TabBar({ state, descriptors, navigation }: BottomTabBarProps) {
   const [dimensions, setDimensions] = useState({ width: 100, height: 20 });
-  const buttonWidth = dimensions.width / state.routes.length
+  const buttonWidth = dimensions.width / state.routes.length;
 
   useEffect(() => {
-    tabPositionX.value = withTiming(buttonWidth * state.index, { duration: 200 });
+    tabBositionX.value = withTiming(buttonWidth * state.index, {
+      duration: 200,
+    });
   }, [state.index]);
-
   const onTabBarLayout = (e: LayoutChangeEvent) => {
     setDimensions({
       height: e.nativeEvent.layout.height,
       width: e.nativeEvent.layout.width,
-    })
-  }
-  const tabPositionX = useSharedValue(0);
-  const animatedStyle = useAnimatedStyle(() => {
-      return {
-          transform: [{ translateX: tabPositionX.value }]
-      }
-  })
+    });
+  };
+  const tabBositionX = useSharedValue(0);
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: tabBositionX.value }],
+  }));
   return (
-    <View style={styles.tabBar}>
+    <View onLayout={onTabBarLayout} style={styles.tabBar}>
       <Animated.View
-        onLayout={onTabBarLayout}
-        style={[animatedStyle,{
-          position: 'absolute',
-          backgroundColor: Colors.primary,
-          top: 0,
-          left: 20,
-          height: 3,
-          width: buttonWidth / 2,
-        }]}
+        style={[
+          animatedStyle,
+          {
+            position: "absolute",
+            backgroundColor: Colors.primary,
+            top: 0,
+            left: 20,
+            height: 2,
+            width: buttonWidth / 2,
+          },
+        ]}
       />
       {state.routes.map((route, index) => {
-        const { options } = descriptors[route.key]
+        const { options } = descriptors[route.key];
         const label =
           options.tabBarLabel !== undefined
             ? options.tabBarLabel
             : options.title !== undefined
             ? options.title
-            : route.name
+            : route.name;
 
-        const isFocused = state.index === index
+        const isFocused = state.index === index;
 
         const onPress = () => {
           const event = navigation.emit({
-            type: 'tabPress',
+            type: "tabPress",
             target: route.key,
             canPreventDefault: true,
-          })
+          });
 
           if (!isFocused && !event.defaultPrevented) {
-            navigation.navigate(route.name, route.params)
+            navigation.navigate(route.name, route.params);
           }
-        }
+        };
 
         const onLongPress = () => {
           navigation.emit({
-            type: 'tabLongPress',
+            type: "tabLongPress",
             target: route.key,
-          })
-        }
+          });
+        };
 
         return (
           <TabBarButton
             key={route.name}
-            isFocused={isFocused}
             onPress={onPress}
             onLongPress={onLongPress}
+            isFocused={isFocused}
             label={label}
             routeName={route.name}
           />
-        )
+        );
       })}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   tabBar: {
-    flexDirection: 'row',
+    flexDirection: "row",
     paddingTop: 16,
     paddingBottom: 40,
     backgroundColor: Colors.white,
   },
-})
+});
